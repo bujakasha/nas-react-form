eb23d41571665dfb9701ccbf021efc9f397ba849
Casper Väresmaa <casper.varesmaa@gmail.com>
2019-05-09 09:59:25 +0300

    init

---
+++ build/precache-manifest.49b0b81985b14ebdd04d4fc6efa3b9a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51dfe342e0c909e471e61fcf4637050f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1ef2f3bce5e9d286035",
+    "url": "/static/css/main.41111cf5.chunk.css"
+  },
+  {
+    "revision": "83e4dbfa4626df5ba36e",
+    "url": "/static/js/2.c8f69ba3.chunk.js"
+  },
+  {
+    "revision": "f1ef2f3bce5e9d286035",
+    "url": "/static/js/main.aa024df3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
